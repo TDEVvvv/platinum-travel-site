8b742abb3a27f8e31dd59c80dcb1b3f06fe4b9cf
Temuge <93143245+Temugeee@users.noreply.github.com>
2023-01-03 16:50:00 +0800

    desktop mobile header style is done

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,20 +1,17 @@
 class MobileMenu {
   constructor() {
-    this.menuIcon = document.querySelector(".site-header__menu-icon");
+    this.menuicon = document.querySelector(".site-header__menu-icon");
     this.menuContent = document.querySelector(".site-header__menu-content");
     this.siteHeader = document.querySelector(".site-header");
     this.events();
   }
-
   events() {
-    this.menuIcon.addEventListener("click", () => this.toggleTheMenu());
+    this.menuicon.addEventListener("click", () => this.toggleTheMenu());
   }
-
   toggleTheMenu() {
     this.menuContent.classList.toggle("site-header__menu-content--is-visible");
     this.siteHeader.classList.toggle("site-header--is-expanded");
-    this.menuIcon.classList.toggle("site-header__menu-icon--close-x");
+    this.menuicon.classList.toggle("site-header__menu-icon--close-x");
   }
 }
-
 export default MobileMenu;
