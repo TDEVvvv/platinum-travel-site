bdffe2c8fc6d6f10fdb4c256f27ad813be6371a9
Temuge <93143245+Temugeee@users.noreply.github.com>
2023-01-05 20:36:10 +0800

    sticky header is done

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,7 +2,7 @@ import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
-let stickyHeader = StickyHeader();
+let stickyHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 50);
 let mobileMenu = new MobileMenu();
