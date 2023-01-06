ca76240b7a58d4a4f7b68365b2448f8ae83e05f5
Temuge <93143245+Temugeee@users.noreply.github.com>
2023-01-06 16:34:56 +0800

    cloud functin is updated

--- cloud-functions/secret-page.js
+++ cloud-functions/secret-page.js
@@ -1,6 +1,21 @@
 exports.handler = function (event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Хэрэглэгчдийн хуудасанд тавтай морилно уу",
-  });
+  const secretContent = `
+    <h3>Welcome to users page!</h3>
+    <p>Only signed users <strong>contents.</strong></p>`;
+  let body;
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
+  if (body.password == "JavaScript") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent,
+    });
+  } else {
+    callback(null, {
+      statusCode: 401,
+    });
+  }
 };
